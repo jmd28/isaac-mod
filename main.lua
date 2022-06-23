10aa77a59ef0cd54ed51aea16b2eb895f3cb8029
James <jmd28@st-andrews.ac.uk>
2022-06-23 22:07:08 +0100

    add basic dollop

--- main.lua
+++ main.lua
@@ -1,13 +1,33 @@
 local Yisz = RegisterMod("Yisz", 1)
+
 local Game = Game()
 local isDebugging = true
 
 Yisz.Badman = Isaac.GetItemIdByName("Badman")
+Yisz.Dollop = Isaac.GetItemIdByName("Dollop")
 
+---comment
+---@param player EntityPlayer
+---@param flag integer
 function Yisz:onCacheEval(player, flag)
+    -- badman logic
     if player:HasCollectible(Yisz.Badman) then
         if flag == CacheFlag.CACHE_DAMAGE then
             player.Damage = player.Damage + 2;
+        end
+
+    end
+
+    -- dollop logic
+    if player:HasCollectible(Yisz.Dollop) then
+        if flag == CacheFlag.CACHE_FLYING then
+            if math.random(0,1) == 1 then
+                player.CanFly = true
+            else
+                player:Kill()
+            end
+        elseif flag == CacheFlag.CACHE_DAMAGE then
+            player.Damage = (player.Damage + 0.5) * 1.5;
         end
     end
 end
@@ -21,6 +41,14 @@ function Yisz:onUpdate(player)
             PickupVariant.PICKUP_COLLECTIBLE,
             Yisz.Badman,
             Vector(320,300),
+            Vector(0,0),
+            nil
+        )
+        Isaac.Spawn(
+            EntityType.ENTITY_PICKUP,
+            PickupVariant.PICKUP_COLLECTIBLE,
+            Yisz.Dollop,
+            Vector(370,350),
             Vector(0,0),
             nil
         )
